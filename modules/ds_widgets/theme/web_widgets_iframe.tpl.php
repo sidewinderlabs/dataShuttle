bfbb6bee8e85ba226cb4f1e388ea9a600d57233c
John Griffin <jg2950@gmail.com>
2011-07-14 01:07:42 +0100

    Add ds_widgets module to bend web_widgets to our will, based on mn_widgets.

---
+++ modules/ds_widgets/theme/web_widgets_iframe.tpl.php
@@ -0,0 +1,18 @@
+<?php
+// $Id: web_widgets_iframe.tpl.php,v 1.1 2009/06/24 07:38:58 aronnovak Exp $
+/**
+ * @file
+ * Template for the code what to embed in external sites
+ */
+?>
+<div style="width:300px; font-family:'Helvetica Neue', Helvetica, Arial, sans-serif; line-height:20px">
+<div style="background-color:#333; padding:0px 5px; font-weight:bold">
+<div style="color:#fff; font-size:14px; line-height:25px;"><?php print $title ?></div>
+<div style="font-size:11px; color:#ccc"><?php print $site_name ?></div>
+</div>
+<script type="text/javascript">
+widgetContext = <?php print $js_variables ?>;
+</script>
+<div id="<?php print $wid ?>"></div>
+<script src="<?php print $script_url ?>"></script>
+</div>
