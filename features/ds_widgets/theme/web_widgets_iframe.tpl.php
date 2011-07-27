b8ac1f5dea4d59b87b6b7d7ab5cd4d3741d8c293
John Griffin <jg2950@gmail.com>
2011-07-28 00:43:18 +0100

    Resize widgets, add tabs

--- features/ds_widgets/theme/web_widgets_iframe.tpl.php
+++ features/ds_widgets/theme/web_widgets_iframe.tpl.php
@@ -5,14 +5,16 @@
  * Template for the code what to embed in external sites
  */
 ?>
-<div style="width:300px; font-family:'Helvetica Neue', Helvetica, Arial, sans-serif; line-height:20px">
-<div style="background-color:#333; padding:0px 5px; font-weight:bold">
+<div style="width:600px; font-family:'Helvetica Neue', Helvetica, Arial, sans-serif; line-height:20px">
+<div style="background-color:#000; padding:0px 5px; font-weight:bold">
 <div style="color:#fff; font-size:14px; line-height:25px;"><?php print $title ?></div>
 <div style="font-size:11px; color:#ccc"><?php print $site_name ?></div>
+<div class="display-tabs" style="font-size:11px; color:#fff"><p>Display: <?php print $tabs ?></p></div>
 </div>
 <script type="text/javascript">
 widgetContext = <?php print $js_variables ?>;
 </script>
 <div id="<?php print $wid ?>"></div>
 <script src="<?php print $script_url ?>"></script>
+<script src="<?php print $tabs_script_url ?>"></script>
 </div>
